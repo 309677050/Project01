da3a14d0507ea560398ed475149985965603dcc5
zhaoyue <309677050@qq.com>
2020-02-15 10:44:59 +0800

    Initial commit

---
+++ Module01/src/com/atguigu/bean/Customer.java
@@ -0,0 +1,11 @@
+package com.atguigu.bean;
+
+/**
+ * @author 赵月
+ * @date 2020年02月13日 12:45
+ */
+public class Customer {
+    public static void main(String[] args) {
+        System.out.println("helloworld!!!");
+    }
+}
---
+++ src/com/atguigu/java/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.atguigu.java;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * @author 赵月
+ * @date 2020年02月13日 12:39
+ */
+public class HelloWorld {
+
+
+
+    public static void main(String[] args) {
+
+
+
+        System.out.println("helloworld!!!");
+        ArrayList list = new ArrayList();
+
+        Date date = new Date();
+
+    }
+}
